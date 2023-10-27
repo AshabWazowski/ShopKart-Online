968ba7fca4029586dc4ce30ce3ec99f23acf9ea6
Ashab <ashab340.abidi@gmail.com>
2023-10-27 14:36:56 +0530

    Server Code

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,60 @@
+// @ts-nocheck
+'use strict';
+
+/**
+ * order controller
+*/
+const { createCoreController } = require('@strapi/strapi').factories;
+
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY )
+
+module.exports = createCoreController('api::order.order', ({ strapi }) => ({
+    async create(ctx){
+        const {products , userName, email} = ctx.request.body;
+        
+        try{
+            
+            // retrieve item information 
+            const lineItems = await Promise.all(
+                products.map(async(product)=>{
+                    const item = await strapi.service("api::item.item").findOne(product.id);
+
+                    return{
+                        price_data:{
+                            currency: "INR",
+                            product_data:{
+                                name: item.name
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        quantity : product.count,
+                    }
+                })
+            );
+
+            // Create stripe session
+
+            const session = await stripe.checkout.session.create({
+                payment_method_types : ["card"],
+                customer_email : email,
+                mode: "payment",
+                success_url : "http://localhost:3000/checkout/success",
+                cancel_url : "http://localhost:3000",
+                line_items: lineItems
+            });
+                // Create the item
+
+                await strapi.service("api::order.order").create({data:{userName, products, stripeSessionId: session.id},
+                });
+
+                return {id: session.id}
+        } 
+        catch(error){
+                ctx.response.status = 500;
+                return {
+                    error: {message: error.message}
+                }
+        }
+    }
+}));
